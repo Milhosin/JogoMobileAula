be597cabed52ba8361f3e9283d1a6b97468c9ebf
milhosin <milho.of@gmail.com>
2026-03-22 14:51:11 -0300

    Adição de Obstaculos e da Camera no Player

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -2,17 +2,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //public
+    [Header("Lerp")]
+    public Transform target;
+    public float lerpSpeed = 1f;
+
     public float speed = 1f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public string tagToCheckEnemy = "Enemy";
+
+    //private
+    private bool _canRun;
+    private Vector3 _pos;
+
+    private void Start()
     {
-        
+        _canRun = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (!_canRun) return;
+
+
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == tagToCheckEnemy)
+        {
+            _canRun = false;
+        }
     }
 }
