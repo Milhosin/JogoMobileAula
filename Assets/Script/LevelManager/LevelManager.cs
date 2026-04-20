2bb8d499eea9e65bf68af6dd2548e3d4783b9a03
milhosin <milho.of@gmail.com>
2026-04-20 19:16:23 -0300

    Adição de troca de level (teste)

--- Assets/Script/LevelManager/LevelManager.cs
+++ Assets/Script/LevelManager/LevelManager.cs
@@ -1,16 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Transform container;
+
+    public List<GameObject> levels;
+
+    [SerializeField] private int _index;
+    private GameObject _currentLevel;
+
+    private void Awake()
     {
-        
+        SpawnNextLevel();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnNextLevel()
     {
-        
+        if(_currentLevel != null)
+        {
+            Destroy(_currentLevel);
+            _index++;
+
+            if(_index >= levels.Count)
+            {
+                ResetLevelIndex();
+            }
+        }
+
+        _currentLevel = Instantiate(levels[_index], container);
+        _currentLevel.transform.localPosition = Vector3.zero;
+    }
+
+    private void ResetLevelIndex()
+    {
+        _index = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SpawnNextLevel();
+        }
     }
 }
